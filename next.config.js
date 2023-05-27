4223df6bb84c3ca9dc88aab9366e470f447b8eef
Anderson Leite <anderson07102010@hotmail.com>
2023-05-27 19:24:01 -0300

    :construction: Testet rotas api

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/status/:code/(.*)',
+        destination: '/api/status/[code]/route.ts',
+      },
+      {
+        source: '/api/users/(.*)',
+        destination: '/api/users/route.ts',
+      },
+      // Adicione outras regras de roteamento para suas rotas handlers aqui
+    ];
+  },
+}
 module.exports = nextConfig
